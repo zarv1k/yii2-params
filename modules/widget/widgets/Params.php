d00d50ba3568a76825dd2266db7b38dfab3d3b93
Dmitry Zarva <zarv1k@gmail.com>
2015-01-08 18:40:50 +0300

    Params widget + widget update module

---
+++ modules/widget/widgets/Params.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace zarv1k\params\modules\widget\widgets;
+
+use yii\base\Widget;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\widgets\ActiveField;
+use yii\widgets\ActiveForm;
+
+class Params extends Widget
+{
+    // TODO: add getter/setter for moduleId + set it in config + move the all options in configuration
+    protected $_moduleId = 'params-widget';
+
+    protected $_submitContent = 'Update';
+    protected $_submitOptions = [
+        'class' => 'btn btn-success btn-sm'
+    ];
+
+    public function run()
+    {
+        /** @var \zarv1k\params\models\DynamicParam[] $models */
+        $models = \zarv1k\params\models\Params::getDynamicModels();
+        $form = ActiveForm::begin([
+            'action' => \Yii::$app->getUrlManager()->createUrl("$this->_moduleId/update")
+        ]);
+        /** @var ActiveField $activeField */
+        $activeField = \Yii::$container->get('yii\widgets\ActiveField'); // TODO: review get from di
+
+        foreach ($models as $model) {
+            echo $form->field($model, "[{$model->owner->id}]{$model->owner->code}", [
+                'labelOptions' => ArrayHelper::merge($activeField->labelOptions,[
+                    'label' => $model->owner->name,
+                ]),
+                'inputOptions' => ArrayHelper::merge($activeField->inputOptions,[
+                    'placeholder' => $model->owner->description,
+                    'title' => $model->owner->description,
+                ]),
+            ]);
+        }
+        echo Html::submitButton($this->getSubmitContent(), $this->getSubmitOptions());
+
+        ActiveForm::end();
+    }
+
+    /**
+     * @return array
+     */
+    public function getSubmitOptions()
+    {
+        return $this->_submitOptions;
+    }
+
+    /**
+     * @param array $submitOptions
+     */
+    public function setSubmitOptions($submitOptions)
+    {
+        $this->_submitOptions = $submitOptions;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubmitContent()
+    {
+        return $this->_submitContent;
+    }
+
+    /**
+     * @param string $submitContent
+     */
+    public function setSubmitContent($submitContent)
+    {
+        $this->_submitContent = $submitContent;
+    }
+}
